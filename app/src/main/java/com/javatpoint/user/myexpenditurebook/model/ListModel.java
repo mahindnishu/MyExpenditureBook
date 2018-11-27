7f79699a07b90e6c1ace57498c4579a9ebf57e0c
mahindnishu <mahindnishu1997@gmail.com>
2018-11-27 13:29:33 +0530

    initial commit

---
+++ app/src/main/java/com/javatpoint/user/myexpenditurebook/model/ListModel.java
@@ -0,0 +1,47 @@
+package com.javatpoint.user.myexpenditurebook.model;
+
+public class ListModel {
+    int id;
+    String date;
+    String expendOn;
+    String expendMoney;
+
+    public ListModel(int id, String date, String expendOn, String expendMoney) {
+        this.id=id;
+        this.date=date;
+        this.expendMoney=expendMoney;
+        this.expendOn=expendOn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExpendOn() {
+        return expendOn;
+    }
+
+    public void setExpendOn(String expendOn) {
+        this.expendOn = expendOn;
+    }
+
+    public String getExpendMoney() {
+        return expendMoney;
+    }
+
+    public void setExpendMoney(String expendMoney) {
+        this.expendMoney = expendMoney;
+    }
+}
---
+++ app/src/main/java/com/javatpoint/user/myexpenditurebook/model/StatusModel.java
@@ -0,0 +1,27 @@
+package com.javatpoint.user.myexpenditurebook.model;
+
+public class StatusModel {
+    String Category;
+    long amount;
+
+    public StatusModel(String category, long amount) {
+        Category = category;
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+}
